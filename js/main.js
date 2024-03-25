7b73d0e8b1d2a0c22837bd614cfdb62c69f26f08
annahico <annahico@gmail.com>
2024-03-25 19:34:10 +0100

    practicando prompt

--- js/main.js
+++ js/main.js
@@ -1,112 +1,145 @@
 
-// // console.log("Hola, soy console log.");
+// // // // console.log("Hola, soy console log.");
 
-// // console.log("Hola, soy otro console log.");
+// // // // console.log("Hola, soy otro console log.");
 
-// // let numero = 55;
-// // const numero2 = 100;
+// // // // let numero = 55;
+// // // // const numero2 = 100;
 
-// // numero = 44;
+// // // // numero = 44;
 
-// // numero = 22;
+// // // // numero = 22;
 
-// // numero = 53;
+// // // // numero = 53;
 
-// // console.log(numero) //let es una variable, por lo que saldrá el último numero antes del console.log//
-// // console.log(numero2)
+// // // // console.log(numero) //let es una variable, por lo que saldrá el último numero antes del console.log//
+// // // // console.log(numero2)
 
-// // // let numero = 290; //estaría mal porque el "numero" ya esta pillado con anterioridad//
+// // // // // let numero = 290; //estaría mal porque el "numero" ya esta pillado con anterioridad//
 
-// //////////JAVASCRIPT 25/03/2024/////////// DECLARACIÓN DE VARIABLE Y OPERACIONES BÁSICAS
+// // // //////////JAVASCRIPT 25/03/2024/////////// DECLARACIÓN DE VARIABLE Y OPERACIONES BÁSICAS
 
-// // let numero1 = 500;
+// // // // let numero1 = 500;
 
-// // let numero2 = 455;
+// // // // let numero2 = 455;
 
-// // console.log(numero1 + numero2); //no quedará fijo porque no se ha puesto el resultado//
+// // // // console.log(numero1 + numero2); //no quedará fijo porque no se ha puesto el resultado//
+
+// // // // let resultado = numero1 + numero2;
+
+// // // // resultado = 100;
+
+// // // // console.log(resultado)
+
+// // // // OPERACIONES BÁSICAS ARITMÉTICAS//
+
+// // // let numero1 = 100;
+// // // let numero2 = 500;
+
+// // // let resultado = numero1 - numero2
+// // // console.log("El resultado de la resta es.......", resultado)
+
+// // // resultado = numero1 + numero2
+// // // console.log("El resultado de la suma es...", resultado)
+
+// // // resultado = numero1 * numero2
+// // // console.log("El resultado de la multiplicación es....", resultado)
+
+// // // resultado = numero1 / numero2
+// // // console.log("El resultado de la división es.....", resultado)
+
+// // // //MODULO
+
+// // // resultado = 17 % 2;
+// // // console.log("El resultado del módulo es.....", resultado)
+
+// // // //Cerciorate de si un número es par o impar
+
+// // // if (resultado !==0){
+
+// // //     console.log("No se trata de un número par")
+
+// // // }
+
+// // // //Pràctica 
+
+// // // let numero4 = 12;
+// // // let numero5 = 7;
+// // // let numero6 = 20;
+
+// // // // Verificar si el primer número es par o impar
+// // // if (numero1 % 2 === 0) {
+// // //     console.log(numero1 + " es un número par.");
+// // //   } else {
+// // //     console.log(numero1 + " es un número impar.");
+// // //   }
+  
+// // //   // Verificar si el segundo número es par o impar
+// // //   if (numero2 % 2 === 0) {
+// // //     console.log(numero2 + " es un número par.");
+// // //   } else {
+// // //     console.log(numero2 + " es un número impar.");
+// // //   }
+  
+// // //   // Verificar si el tercer número es par o impar
+// // //   if (numero3 % 2 === 0) {
+// // //     console.log(numero3 + " es un número par.");
+// // //   } else {
+// // //     console.log(numero3 + " es un número impar.");
+// // //   }
+
+// // //ESTRUCTURA DE CONTROL DE PROGRAMACIÓN CONDICIONAL "IF"
+
+// // let nombre = "Pepe"
+
+// // if (nombre ==="Daniel"){
+
+// //     console.log("Hola Daniel")
+// // }else{
+
+// //     console.log("Tu no eres Daniel")
+// // }
+
+// // let numero1 = 10;
+
+// // let numero2 = 35;
 
 // // let resultado = numero1 + numero2;
 
-// // resultado = 100;
+// // if (resultado > 80){
 
-// // console.log(resultado)
+// //     console.log("Si, el resultado es mayor que 28")
+// // }
 
-// // OPERACIONES BÁSICAS ARITMÉTICAS//
+// //OPERACIONES ARITMETICAS DE INCREMENTO Y DECREMENTO
 
-// let numero1 = 100;
-// let numero2 = 500;
+// let numero = 5;
 
-// let resultado = numero1 - numero2
-// console.log("El resultado de la resta es.......", resultado)
+// numero = numero + 1  //numero++;
+// console.log(numero)
 
-// resultado = numero1 + numero2
-// console.log("El resultado de la suma es...", resultado)
 
-// resultado = numero1 * numero2
-// console.log("El resultado de la multiplicación es....", resultado)
+// numero = numero - 1 //numero--
+// console.log(numero) 
 
-// resultado = numero1 / numero2
-// console.log("El resultado de la división es.....", resultado)
+// let numero = prompt("Dime el número que quieras");
 
-// //MODULO
+// if (numero >10){
 
-// resultado = 17 % 2;
-// console.log("El resultado del módulo es.....", resultado)
-
-// //Cerciorate de si un número es par o impar
-
-// if (resultado !==0){
-
-//     console.log("No se trata de un número par")
-
+//     console.log("El número es mayor a 10.")
 // }
 
-// //Pràctica 
+//proposición de ejercicio
 
-// let numero4 = 12;
-// let numero5 = 7;
-// let numero6 = 20;
+// Pedir al usuario que ingrese un número
+let numero = prompt("Ingresa un número:");
 
-// // Verificar si el primer número es par o impar
-// if (numero1 % 2 === 0) {
-//     console.log(numero1 + " es un número par.");
-//   } else {
-//     console.log(numero1 + " es un número impar.");
-//   }
-  
-//   // Verificar si el segundo número es par o impar
-//   if (numero2 % 2 === 0) {
-//     console.log(numero2 + " es un número par.");
-//   } else {
-//     console.log(numero2 + " es un número impar.");
-//   }
-  
-//   // Verificar si el tercer número es par o impar
-//   if (numero3 % 2 === 0) {
-//     console.log(numero3 + " es un número par.");
-//   } else {
-//     console.log(numero3 + " es un número impar.");
-//   }
+// Convertir la entrada del usuario a un número entero
+let numeroEntero = parseInt(numero);
 
-//ESTRUCTURA DE CONTROL DE PROGRAMACIÓN CONDICIONAL "IF"
-
-let nombre = "Pepe"
-
-if (nombre ==="Daniel"){
-
-    console.log("Hola Daniel")
-}else{
-
-    console.log("Tu no eres Daniel")
-}
-
-let numero1 = 10;
-
-let numero2 = 35;
-
-let resultado = numero1 + numero2;
-
-if (resultado > 80){
-
-    console.log("Si, el resultado es mayor que 28")
+// Verificar si el número es par o impar
+if (numeroEntero % 2 === 0) {
+  console.log(numeroEntero + " es un número par.");
+} else {
+  console.log(numeroEntero + " es un número impar.");
 }
