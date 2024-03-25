7a9871743a76fc45e81c12312524494798443d0c
annahico <annahico@gmail.com>
2024-03-25 19:06:07 +0100

    prira practica/teoria de js

---
+++ js/main.js
@@ -0,0 +1,55 @@
+
+// console.log("Hola, soy console log.");
+
+// console.log("Hola, soy otro console log.");
+
+// let numero = 55;
+// const numero2 = 100;
+
+// numero = 44;
+
+// numero = 22;
+
+// numero = 53;
+
+// console.log(numero) //let es una variable, por lo que saldrá el último numero antes del console.log//
+// console.log(numero2)
+
+// // let numero = 290; //estaría mal porque el "numero" ya esta pillado con anterioridad//
+
+//////////JAVASCRIPT 25/03/2024/////////// DECLARACIÓN DE VARIABLE Y OPERACIONES BÁSICAS
+
+// let numero1 = 500;
+
+// let numero2 = 455;
+
+// console.log(numero1 + numero2); //no quedará fijo porque no se ha puesto el resultado//
+
+// let resultado = numero1 + numero2;
+
+// resultado = 100;
+
+// console.log(resultado)
+
+// OPERACIONES BÁSICAS ARITMÉTICAS//
+
+let numero1 = 100;
+let numero2 = 500;
+
+let resultado = numero1 - numero2
+console.log("El resultado de la resta es.......", resultado)
+
+resultado = numero1 + numero2
+console.log("El resultado de la suma es...", resultado)
+
+resultado = numero1 * numero2
+console.log("El resultado de la multiplicación es....", resultado)
+
+resultado = numero1 / numero2
+console.log("El resultado de la división es.....", resultado)
+
+//MODULO
+
+resultado = 10 % 2;
+console.log("El resultado del módulo es.....", resultado)
+
